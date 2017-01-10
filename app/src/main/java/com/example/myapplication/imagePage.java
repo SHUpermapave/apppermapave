254b0f98b2f8907b5959adcbd0de62b37722c0f6
Kazi2015 <aranbacall@hotmail.co.uk>
2017-01-10 07:42:44 +0000

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/imagePage.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class imagePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_page);
+    }
+}
